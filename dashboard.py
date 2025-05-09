cb91c35c3c299504577a7ac5de1b315878c1e15c
Kevin <130358969+Kevinbllt@users.noreply.github.com>
2025-05-09 14:17:01 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -181,7 +181,7 @@ if not df_avg.empty:
 st.markdown(
     f"""
     <div style='text-align: center; color: grey; margin-top: 50px; font-size: 0.9em;'>
-        🕑 This dashboard is updated every day at <b>10am Toronto time</b> — last checked: {datetime.now().strftime('%H:%M:%S')}
+        🕑 This dashboard is updated every day at <b>10am Toronto time</b>
     </div>
     """,
     unsafe_allow_html=True
